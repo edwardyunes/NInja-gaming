5dfdc5e900d2796755ffa8bc989b9cc8db9f325e
edwardyunes <edwardsyunes@gmail.com>
2022-09-14 12:01:22 -0400

    initial

---
+++ src/routes/about.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+        
+
+<div class="about">
+
+    <h2> About Us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia tenetur eaque explicabo inventore perspiciatis dolor quam eius ut quaerat quibusdam, adipisci cum harum aspernatur hic quo tempora excepturi natus cumque deserunt magni consequatur quisquam maxime ipsum. Sapiente molestiae harum, doloremque nihil cum id veritatis atque velit veniam dolores, expedita, est numquam. Doloribus quas quo tempore at vitae necessitatibus explicabo vel ea fuga ipsam perspiciatis sequi iusto neque obcaecati, facere doloremque quisquam possimus soluta commodi. Reiciendis necessitatibus soluta quam! Vel, voluptatem.</p>
+    <a href="/guides">View Guides</a>
+    <a href="/">home</a>
+</div>
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+    
+    </style>
+    
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+
+<script>
+
+</script>
+
+
+
+<div class="index">
+    <h2>Welcome</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam totam minima corporis, hic laudantium cum accusamus iusto molestias laborum iste, ea id maiores non ipsum facere aliquam. Incidunt velit cum nemo dolores iure ad similique. Eos, cumque eaque repudiandae magni nihil cum dolorem illo fugit impedit consequatur officia. Deleniti, modi ipsa asperiores non a tenetur. Quaerat incidunt rem delectus culpa?</p>
+
+<a href="/guides">View Guides</a>
+<a href="/about">About</a>
+
+
+</div>
+
+<style>
+.index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+}
+
+</style>
